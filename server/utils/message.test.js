09bc0b9eebac577cc33b680c977c12500264881d
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-10-10 21:47:14 +0700

    Add test case generateLocationMessage

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,24 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object',() => {
         var from = 'ton';
         var text = 'hello test generateMessage';
-        var message = generateMessage(from,text)
+        var message = generateMessage(from,text);
         expect(message.createAt).toBeA('number');
         expect(message).toInclude({from,text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct LocationMessage object',() => {
+        var from = 'ton';
+        var latitude  = '15.13';
+        var longitude = '-17.123';
+        var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+        var locationMessage = generateLocationMessage(from , latitude, longitude);
+        expect(locationMessage.createAt).toBeA('number');
+        expect(locationMessage).toInclude({from,url});
     });
 });
