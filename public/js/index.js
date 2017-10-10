5feadbff231458fc6584197a903884b68f159f46
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-10-10 23:37:09 +0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,11 +26,12 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+    var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage',{
         from : 'User',
         text: jQuery('[name=message]').val()
     }, function () {
-
+        messageTextbox.val('')
     });
 });
 
@@ -39,12 +40,15 @@ locationButton.on('click', function () {
     if(!navigator.geolocation){
         return alert('Geolocation bot supported by your browser.');
     }
+    locationButton.attr('disabled','disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition( function(position) {
-       socket.emit('createlocationMessage', {
+        locationButton.removeAttr('disabled').text('Send location');
+        socket.emit('createlocationMessage', {
            latitude : position.coords.latitude,
            longitude : position.coords.longitude
-       });
+        });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
