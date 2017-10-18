c44c27485e03d0de1c03c5abc485adfc417cd47d
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-10-19 00:29:42 +0700

    Add Join room and class person

---
+++ server/utils/users.test.js
@@ -0,0 +1,16 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {
+    it('should add new user', () => {
+        var users = new Users();
+        var user = {
+            id : '123',
+            name : 'abc',
+            room : 'the office Fans'
+        };
+        var resUser = users.addUser(user.id,user.name,user.room);
+        expect(users.users).toEqual([user])
+    });
+});
